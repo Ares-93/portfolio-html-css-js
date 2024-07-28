e8747cb202848fbf982b31847707e492d789b5b9
Mohammad Haider <arzhdr@gmail.com>
2024-07-28 13:36:33 -0400

    added smooth scrool to nav links

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function toggleMobileMenu() {
   document.getElementById("menu").classList.toggle("active");
 }
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
